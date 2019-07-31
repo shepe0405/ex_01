b4b034d8779ca878f71306794c6f1d9101828b4c
shepe0405 <pedrohf96@hotmail.com>
2019-07-31 11:27:22 -0300

    Primeiro commit Teste

---
+++ ex_01.php
@@ -0,0 +1,143 @@
+<?php 
+
+/*
+Exercicios - 01
+	RECEBA DUAS NOTAS DE PROVA
+	TIRE A MEDIA DELAS E EXIBA UMA MENSAGEM DE DESEMPENHO DE ACORDO COM A NOTA
+		DESEMPENHOS
+			PESSIMO
+			RUIM
+			MEDIO
+			BOM
+			EXCELENTE
+
+	UMA PÁGINA
+		FORM MANDANDO PRA SI MESMO POR POST
+			MATERIALIZE OU BOOTSTRAP
+				DOIS CAMPOS INPUTS TIPO TEXTO MAS ACEITANDO APENAS NUMEROS
+			BOTAO SUBMIT - TIRAR ENTER DE SUBMIT
+		PROCESSAR RECEBENDO AS NOTAS POR POST
+		TIRAR A MEDIA
+		APRESENTAR COM IF OU SWITCH OS DESEMPENHOS
+			CADA DESEMPENHO TEM MENSAGEM E ICONE
+		APRESENTAÇÃO
+			FORM SOME
+			NO LUGAR APARECE SOMENTE O DESEMPENHO COM ICONE E AS NOTAS RECEBIDAS
+			BOTÃO CALUCLAR NOVAMENTE
+			DESEMPENHO SOME
+
+			cores
+				#616161 grey darken 2
+				#fff9c4 yellow lighten-4
+				#ff5252 red accent-2
+				#4E9BCC
+				#4D8BB3
+*/
+?>
+<!--DOCTYPE html-->
+<html>
+<head lang="pt-br">
+	<title>Média</title>
+   <?php require_once "header.php"; ?>
+   <style type="text/css">
+   	nav {
+   		background-color: #4D8BB3;
+   	}
+   	body {
+   		color: #616161;
+   	}
+   	/*MESMA COISA QUE O ANTERIOR POREM QUANDO NAO ESTA EM FOCO PELOS TIPOS DE INPUT*/
+	.input-field input[type=text]:focus:not([readonly]) + label, .input-field input[type=email]:focus:not([readonly]) + label, textarea.materialize-textarea:focus:not([readonly]) + label, .input-field input[type=password]:focus:not([readonly]) + label  {
+	    color: #ff5252;
+	 }
+	/* label underline focus color */
+	.input-field input[type=text]:focus:not([readonly]), .input-field input[type=email]:focus:not([readonly]), .input-field input[type=password]:focus:not([readonly]) {
+	    border-bottom: 1px solid #ff5252;
+	    box-shadow: 0 1px 0 0 #ff5252;
+	 }
+	/* icon prefix focus color */
+	.input-field .prefix.active {
+	    color: #ff5252;
+	}
+   /* label focus color */
+   .input-field input[type=text]:focus + label {
+     color: #ff5252;
+   }
+   /* label underline focus color */
+   .input-field input[type=text]:focus {
+     border-bottom: 1px solid #ff5252;
+     box-shadow: 0 1px 0 0 #ff5252;
+   }
+   /* valid color */
+   .input-field input[type=text].valid {
+     border-bottom: 1px solid #ff5252;
+     box-shadow: 0 1px 0 0 #ff5252;
+   }
+   /* invalid color */
+   .input-field input[type=text].invalid {
+     border-bottom: 1px solid #ff5252;
+     box-shadow: 0 1px 0 0 #ff5252;
+   }
+   /* icon prefix focus color */
+   .input-field .prefix.active {
+     color: #ff5252;
+   }
+   </style>
+<div class="container">
+	<form action="" method="post" name="media">
+		<h4 class="center">Média</h4>
+		<div class="row">
+			<div class="input-field col s6">
+				<i class="material-icons prefix">filter_1</i>
+					<input type="text" id="nota01" name="nota01" placeholder="Digite o Valor da Primeira Nota">
+				<label>Nota</label>
+			</div>
+			<div class="input-field col s6">
+				<i class="material-icons prefix">filter_2</i>
+					<input type="text" name="nota02" id="nota02" placeholder="Digite o Valor da Segunda Nota">
+				<label>Nota</label>
+			</div>
+		</div>
+		<div class="center">
+			<button type="submit" onclick="return validar()" name="calcular" class="btn waves-effect waves-light red accent-2">Calcular</button>
+		</div>
+	</form>
+</div>
+
+<?php 
+	if (isset($_POST['calcular'])) {
+		$nota01 = $_POST['nota01'];
+		$nota02 = $_POST['nota02'];
+		echo $nota01;
+	}
+
+?>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+    <script type="text/javascript">
+    	 $(document).ready(function(){
+		    $('.sidenav').sidenav();
+		  });
+    	 function validar()
+    	 {
+    	 	var nota01 = media.nota01.value;
+    	 	var nota02 = media.nota02.value;
+    	 	if (nota01 < 0 || nota01 > 10) {
+    	 		M.toast({html: 'Campo deve conter uma nota válida entre 0 e 10'});
+    	 		media.nota01.focus();
+    	 		return false;
+    	 	}
+    	 	if (nota02 < 0 || nota02 > 10) {
+    	 		M.toast({html: 'Campo deve conter uma nota válida entre 0 e 10'});
+    	 		media.nota02.focus();
+    	 		return false;
+    	 	}
+    	 }
+    	
+    </script>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,19 @@
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+<body>
+	<nav>
+	    <div class="nav-wrapper container">
+	      <a href="#!" class="brand-logo center">Logo</a>
+	      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+	      <ul class="right hide-on-med-and-down">
+	        <li><a href="ex_01.php">Média</a></li>
+	      </ul>
+	    </div>
+	  </nav>
+
+	  <ul class="sidenav" id="mobile-demo">
+	   	<li><a href="ex_01.php">Média</a></li>
+	  </ul>
